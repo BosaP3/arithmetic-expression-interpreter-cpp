230b55a0cb4c6f89ce40f1c0663d0056b33b1f1a
Pedro Antônio da Silva <pedro.dasilva.313@gmail.com>
2023-11-20 11:13:34 -0300

    organização de tarefas fora da main

--- expression_evaluation.cpp
+++ expression_evaluation.cpp
@@ -1,9 +1,39 @@
 //*****test****
 #include <sstream>
+#include <iostream>
+#include <unordered_map>
+#include <cmath>
+#include <string>
+
+std::unordered_map<std::string, double> variables;
 
 bool isNumber(const std::string &s)
 {
     std::istringstream iss(s);
     double value;
     return iss >> value >> std::ws && iss.eof();
+}
+
+void evaluation_Variable(const std::string &input)
+{
+    size_t pos = input.find("="); // encontra o sinal
+    std::string var = input.substr(0, pos);
+    double value = std::stod(input.substr(pos + 1));
+    variables[var] = value; // guarda no map
+
+    // testar se o valor depois do sinal é uma expressao e resolver
+}
+
+void evaluation(const std::string &input)
+{
+    if (isNumber(input)) // teste se for somente um numero
+        std::cout << input << std::endl;
+    else if (input.find('=') != std::string::npos) // teste se é variavel
+        evaluation_Variable(input);
+    else if (variables.find(input) != variables.end()) // teste de chave mapeada
+        std::cout << variables[input] << std::endl;
+    else // teste de expressao
+    {
+        // return evaluation_Expression(input); // Em caso de "input" função...
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <unordered_map>
 #include "expression_evaluation.cpp"
 
 using namespace std;
@@ -8,7 +7,6 @@ using namespace std;
 int main()
 {
     string user_input;
-    unordered_map<string, double> variables;
 
     // Interface REPL (Read-Eval-Print-Loop) *****testing*****
     while (1)
@@ -19,29 +17,7 @@ int main()
         if (user_input == "exit")
             break;
 
-        if (isNumber(user_input)) // teste numero
-        {
-            cout << user_input << endl;
-        }
-        else
-        {
-            size_t pos = user_input.find("=");
-            if (pos != string::npos) // testa variavel
-            {
-                string var = user_input.substr(0, pos);
-                double value = stod(user_input.substr(pos + 1));
-                variables[var] = value;
-            }
-            else if (variables.find(user_input) != variables.end()) // testa variavel existente
-            {
-                cout << variables[user_input] << endl;
-            }
-            else
-            {
-                cout << "Variavel nao definida: " << user_input << endl;
-            }
-        }
-        // expressao
+        evaluation(user_input); // manda a entrada para avaliação
     }
 
     return 0;
